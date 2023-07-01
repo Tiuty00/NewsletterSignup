13c1fdc989ed541a310bfe8c6d33a03dfd4f7dc6
Tommaso <tommy200300@gmail.com>
2023-07-01 07:27:14 +0200

    path js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: require('find-config')('.env') });
+require("dotenv").config({path: require('find-config')('.env')});
 
 const express = require("express");
 const bodyParser = require("body-parser");
