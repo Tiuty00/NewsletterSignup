eae586df29c3e4afdb8e655f315de5977f65410f
Tommaso <tommy200300@gmail.com>
2023-07-01 07:30:46 +0200

    try with this path

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-require("dotenv").config({path: require('find-config')('.env')});
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
 
 const express = require("express");
 const bodyParser = require("body-parser");
