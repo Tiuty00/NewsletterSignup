5fdc34048ee737a471efe7f155a0953ee67c84d8
Tommaso <107317770+Tiuty00@users.noreply.github.com>
2023-07-01 09:08:11 +0200

    Update app.js path

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname + "/.env") })
+const {resolve} = require('path');
+
+require('dotenv').config({path: resolve(__dirname,"../../config/dev.env")});
 
 const express = require("express");
 const bodyParser = require("body-parser");
